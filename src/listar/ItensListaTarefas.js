ea856b8414c459adf1f4838f16ed44264ec85822
Arthur Cezar <arthurcvm@hotmail.com>
2021-01-05 16:37:34 -0300

    Funcionalidade de concluir tarefa

--- src/listar/ItensListaTarefas.js
+++ src/listar/ItensListaTarefas.js
@@ -3,20 +3,29 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { A } from "hookrouter";
+import ConcluirTarefa from "./ConcluirTarefa";
 
 function ItensListaTarefas(props) {
-
-    function marcarConcluida(tarefa) {
-        return tarefa.concluida ? 'line-through' : 'none'
-    }
+  function marcarConcluida(tarefa) {
+    return tarefa.concluida ? "line-through" : "none";
+  }
 
   return props.tarefas.map((tarefa) => (
     <tr key={tarefa.id} data-testid="tarefa">
-      <td width="75%" data-testid="nome-tarefa"
-      style={{textDecoration: marcarConcluida(tarefa)}}>
+      <td
+        width="75%"
+        data-testid="nome-tarefa"
+        style={{ textDecoration: marcarConcluida(tarefa) }}
+      >
         {tarefa.nome}
       </td>
       <td className="text-right">
+        <ConcluirTarefa
+          tarefa={tarefa}
+          recarregarTarefas={props.recarregarTarefas}
+          className={tarefa.concluida ? "hidden" : ""}
+        />
+        &nbsp;
         <A
           href={"/atualizar/" + tarefa.id}
           className={tarefa.concluida ? "hidden" : "btn btn-warning btn-sm"}
